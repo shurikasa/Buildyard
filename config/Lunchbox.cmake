bb386bdeaa713bac098fdf32a7f0194dd8f1dc77
Stefan Eilemann <eilemann@gmail.com>
2012-06-20 11:59:52 +0200

    configure 1.4b release tags

--- config/Lunchbox.cmake
+++ config/Lunchbox.cmake
@@ -1,6 +1,6 @@
 
 set(LUNCHBOX_VERSION 1.3.5)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
-set(LUNCHBOX_REPO_TAG master)
+set(LUNCHBOX_REPO_TAG 1.4)
 set(LUNCHBOX_DEPENDS Boost hwloc eyescale)
 set(LUNCHBOX_FORCE_BUILD ON)
--- config/gpu-sd.cmake
+++ config/gpu-sd.cmake
@@ -1,7 +1,7 @@
 
 set(GPU-SD_VERSION 1.3.5)
 set(GPU-SD_REPO_URL https://github.com/Eyescale/gpu-sd.git)
-set(GPU-SD_REPO_TAG master)
+set(GPU-SD_REPO_TAG 1.4)
 set(GPU-SD_ROOT_VAR GPUSD_ROOT)
 set(GPU-SD_DEPENDS Boost Lunchbox OpenGL)
 set(GPU-SD_FORCE_BUILD ON)
