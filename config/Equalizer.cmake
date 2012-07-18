315aec5ac7d12419b711f813d965785978e2a3ba
Stefan Eilemann <eilemann@gmail.com>
2012-07-18 15:54:24 +0200

    Bump versions for 1.4

--- config/Equalizer.cmake
+++ config/Equalizer.cmake
@@ -1,5 +1,5 @@
 
-set(EQUALIZER_VERSION 1.3.5)
+set(EQUALIZER_VERSION 1.3.6)
 set(EQUALIZER_DEPENDS gpu-sd Boost hwloc vmmlib Lunchbox OpenGL)
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
 set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
--- config/vmmlib.cmake
+++ config/vmmlib.cmake
@@ -1,5 +1,5 @@
 
-set(VMMLIB_VERSION 1.3.5)
+set(VMMLIB_VERSION 1.4.0)
 set(VMMLIB_REPO_URL
   http://vmmlib.svn.sourceforge.net/svnroot/vmmlib/tags/release-1.3.5)
 set(VMMLIB_REPO_TYPE svn)
