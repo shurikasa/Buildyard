5f50522676f64f5fd366d483322a0275f253fcd9
Stefan Eilemann <eilemann@gmail.com>
2012-07-18 15:58:52 +0200

    Fix vmml repo

--- config/vmmlib.cmake
+++ config/vmmlib.cmake
@@ -1,8 +1,7 @@
 
 set(VMMLIB_VERSION 1.4.0)
-set(VMMLIB_REPO_URL
-  http://vmmlib.svn.sourceforge.net/svnroot/vmmlib/tags/release-1.3.5)
-set(VMMLIB_REPO_TYPE svn)
+set(VMMLIB_REPO_URL https://github.com/VMML/vmmlib.git)
+set(VMMLIB_REPO_TAG 1.4)
 set(VMMLIB_NOTEST ON)
 set(VMMLIB_DEPENDS eyescale)
 set(VMMLIB_FORCE_BUILD ON)
