0ec1c9cd72fffad1bfdcf99451abec0ff91da1c1
Stefan Eilemann <eilemann@gmail.com>
2012-09-07 11:18:25 +0200

    Fix gpusd repo

--- config/gpu-sd.cmake
+++ config/gpu-sd.cmake
@@ -1,6 +1,6 @@
 
 set(GPU-SD_VERSION 1.4.0)
-set(GPU-SD_REPO_URL https://github.com/Eyescale/gpu-sd.git)
+set(GPU-SD_REPO_URL https://github.com/Eyescale/gpusd.git)
 set(GPU-SD_REPO_TAG 1.4)
 set(GPU-SD_ROOT_VAR GPUSD_ROOT)
 set(GPU-SD_DEPENDS Boost Lunchbox OpenGL)
