459f626d4c525f0d9d066a26a6268af9ca7147c2
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-06-06 09:20:24 +0200

    Improve autopain script (attempting to build LibTurboJpeg on OS X)

--- CMake/UseExternalAutoconf.cmake
+++ CMake/UseExternalAutoconf.cmake
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2013 Stefan.Eilemann@epfl.ch
 
 # write configure command for autoconf-based projects
@@ -9,15 +8,39 @@ function(USE_EXTERNAL_AUTOCONF name)
   if(NOT ${NAME}_CONFIGURE_DIR)
     set(${NAME}_CONFIGURE_DIR ${CMAKE_BINARY_DIR}/${name})
   endif()
+
+  set(CFLAGS "$ENV{CFLAGS} -I${CMAKE_CURRENT_BINARY_DIR}/install/include")
+  set(CXXFLAGS "$ENV{CXXFLAGS} -I${CMAKE_CURRENT_BINARY_DIR}/install/include")
+  set(LDFLAGS "$ENV{LDFLAGS} -L${CMAKE_CURRENT_BINARY_DIR}/install/lib")
+  if(APPLE)
+    foreach(ARCH ${CMAKE_OSX_ARCHITECTURES})
+      set(CFLAGS "${CFLAGS} -arch ${ARCH}")
+      set(CXXFLAGS "${CXXFLAGS} -arch ${ARCH}")
+    endforeach()
+    if(CMAKE_OSX_SYSROOT)
+      set(CFLAGS "${CFLAGS} -isysroot ${CMAKE_OSX_SYSROOT}")
+      set(CXXFLAGS "${CXXFLAGS} -isysroot ${CMAKE_OSX_SYSROOT}")
+    endif()
+  endif()
+
   file(WRITE ${${NAME}_CONFIGURE_CMD}
     "if(NOT EXISTS ${${NAME}_SOURCE}/configure)\n"
-    "  execute_process(COMMAND autoreconf -i \n"
+    "  execute_process(COMMAND autoreconf -i\n"
     "    WORKING_DIRECTORY ${${NAME}_SOURCE})\n"
     "endif()\n"
     "if(NOT EXISTS ${CMAKE_BINARY_DIR}/${name}/config.status)\n"
-    "  execute_process(COMMAND ${${NAME}_SOURCE}/configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/install CPPFLAGS=-I${CMAKE_CURRENT_BINARY_DIR}/install/include LDFLAGS=-L${CMAKE_CURRENT_BINARY_DIR}/install/lib\n"
-    "    WORKING_DIRECTORY ${${NAME}_CONFIGURE_DIR})\n"
-    "  execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/${name}/config.status)\n"
+    "  set(ENV{CFLAGS} ${CFLAGS})\n"
+    "  set(ENV{CXXFLAGS} ${CXXFLAGS})\n"
+    "  set(ENV{LDFLAGS} ${LDFLAGS})\n"
+    "  execute_process(COMMAND ${${NAME}_SOURCE}/configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/install\n"
+    "    WORKING_DIRECTORY ${${NAME}_CONFIGURE_DIR}\n"
+    "    RESULT_VARIABLE ERROR)\n"
+    "  if(ERROR)\n"
+    "    execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_BINARY_DIR}/${name}/config.status)\n"
+    "    message(FATAL_ERROR \"Configure script for ${name} failed\")\n"
+    "  else()\n"
+    "    execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/${name}/config.status)\n"
+    "  endif()\n"
     "endif()\n"
     )
 endfunction()
