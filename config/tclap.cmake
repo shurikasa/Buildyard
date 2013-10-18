58f2b2c645c12f123766e040241f3aa59b35c1d3
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-10-18 13:12:01 +0200

    Switch TCLAP to our repository with fixes

--- config/tclap.cmake
+++ config/tclap.cmake
@@ -1,10 +1,8 @@
 
-set(TCLAP_PACKAGE_VERSION 1.2)
+set(TCLAP_PACKAGE_VERSION 1.3)
 set(TCLAP_OPTIONAL ON)
-set(TCLAP_REPO_URL git://git.code.sf.net/p/tclap/code)
-set(TCLAP_REPO_TAG tclap-1-2-1)
+set(TCLAP_REPO_URL https://github.com/BlueBrain/tclap.git)
+set(TCLAP_REPO_TAG bbp)
 set(TCLAP_SOURCE "${CMAKE_SOURCE_DIR}/src/tclap")
 set(TCLAP_AUTOCONF ON)
 set(TCLAP_CONFIGURE_FLAGS "--enable-doxygen")
-set(TCLAP_PRE_CONFIGURE
-  "execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/tclap.fix ${TCLAP_SOURCE}/docs/Makefile.am)")
