5a8d84385b65e7d3a58d5ee6b95e01163a5e5452
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-10-18 11:05:40 +0200

    two-step TCLAP->tclap rename (thanks HFS!)

---
+++ config/tclap.cmake
@@ -0,0 +1,10 @@
+
+set(TCLAP_PACKAGE_VERSION 1.2)
+set(TCLAP_OPTIONAL ON)
+set(TCLAP_REPO_URL git://git.code.sf.net/p/tclap/code)
+set(TCLAP_REPO_TAG tclap-1-2-1)
+set(TCLAP_SOURCE "${CMAKE_SOURCE_DIR}/src/tclap")
+set(TCLAP_AUTOCONF ON)
+set(TCLAP_CONFIGURE_FLAGS "--enable-doxygen")
+set(TCLAP_PRE_CONFIGURE
+  "execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_LIST_DIR}/tclap.fix ${TCLAP_SOURCE}/docs/Makefile.am)")
